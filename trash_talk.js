b45de79b35e54f26fb1bb0e20a8954abbe8a91bb
Pin Jin <79257354+pinjin1201@users.noreply.github.com>
2021-08-01 17:10:14 +0800

    Update trash_talk.js

--- trash_talk.js
+++ trash_talk.js
@@ -18,6 +18,21 @@ const phrase = ['很簡單', '沒什麼', '難不倒你', '不算什麼']
 
 // 執行 話語產生器
 function generateTrashTalk(options) {
+  // 錯誤回應 ( 使用者沒有選擇角色 )
+  if (!options) {
+    return '請選擇一個角色!'
+  }
+
+  // 產生亂數
+  const randomNumberForTask = Math.floor(Math.random() * phrase.length)
+  const randomNumberForMock = Math.floor(Math.random() * phrase.length)
+
+  // 判斷使用者選擇的角色，給予回應
+  return `身為一個${target[options]}，${task[options][randomNumberForTask]}，${phrase[randomNumberForMock]}吧 !`
+
+  /*
+  // for in loop ( 當 role 過多時，用字串的方式較 for loop 方式佳 )
+  
   // 產生亂數
   const randomNumber = Math.floor(Math.random () * phrase.length)
   // 新增一空字串
@@ -33,6 +48,7 @@ function generateTrashTalk(options) {
   if (!options) {
     return '請選擇一個角色!'
   }
+  */
 }
 
 module.exports = generateTrashTalk
